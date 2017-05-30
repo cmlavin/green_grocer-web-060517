74c505dee13796d1000046c3129a8efdc4b6f280
Catherine Lavin <cmlavin7@gmail.com>
2017-05-30 17:39:04 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,52 @@
 def consolidate_cart(cart)
-  # code here
+  new_cart = {}
+  cart.each do |item|
+    item.each do |food, hash|
+      if new_cart[food]
+        new_cart[food][:count] += 1
+      else
+        new_cart[food] = hash
+        new_cart[food][:count] = 1
+      end
+    end
+  end
+  new_cart
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon|
+    food = coupon[:item]
+    if cart[food] && cart[food][:count] >= coupon[:num]
+      if cart["#{food} W/COUPON"]
+        cart["#{food} W/COUPON"][:count] += 1
+      else
+        cart["#{food} W/COUPON"] = {:price => coupon[:cost], :clearance => cart[food][:clearance], :count => 1}
+      end
+      cart[food][:count] -= coupon[:num]
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |food, hash|
+    if hash[:clearance]
+      hash[:price] = (hash[:price] * 0.80).round(2)
+    end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  total = 0
+  new_cart = consolidate_cart(cart)
+  new_cart = apply_coupons(new_cart, coupons)
+  new_cart = apply_clearance(new_cart)
+  new_cart.each do |food, hash|
+    total += hash[:price] * hash[:count]
+  end
+  if total > 100
+    total *= 0.90
+  end
+  total
 end
